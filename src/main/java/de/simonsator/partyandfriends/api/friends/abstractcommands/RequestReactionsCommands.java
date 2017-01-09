e3c19fb515edb890a707dc8ae498d5dcadefc2d8
Simon Brungs <00pflaume@gmail.com>
2017-01-09 15:47:31 +0100

    Fixed language mistake

--- src/main/java/de/simonsator/partyandfriends/api/friends/abstractcommands/RequestReactionsCommands.java
+++ src/main/java/de/simonsator/partyandfriends/api/friends/abstractcommands/RequestReactionsCommands.java
@@ -2,8 +2,17 @@ package de.simonsator.partyandfriends.api.friends.abstractcommands;
 
 import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
 import de.simonsator.partyandfriends.api.pafplayers.PAFPlayer;
+import de.simonsator.partyandfriends.friends.commands.Friends;
+import de.simonsator.partyandfriends.main.Main;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.regex.Matcher;
+
+import static de.simonsator.partyandfriends.utilities.PatterCollection.PLAYER_PATTERN;
 
 public abstract class RequestReactionsCommands extends FriendSubCommand {
+	private Matcher playerMatcher = PLAYER_PATTERN.matcher(Main.getInstance().getMessagesYml()
+			.getString("Friends.Command.Accept.ErrorNoFriendShipInvitation"));
 
 	protected RequestReactionsCommands(String[] pCommands, int pPriority, String pHelp) {
 		super(pCommands, pPriority, pHelp);
@@ -11,7 +20,7 @@ public abstract class RequestReactionsCommands extends FriendSubCommand {
 
 	protected boolean hasNoRequest(OnlinePAFPlayer pPlayer, PAFPlayer pQueryPlayer) {
 		if ((!pPlayer.hasRequestFrom(pQueryPlayer))) {
-			sendError(pPlayer, "Friends.Command.Accept.ErrorNoFriendShipInvitation");
+			sendError(pPlayer, new TextComponent(Friends.getInstance().getPrefix() + playerMatcher.replaceFirst(pQueryPlayer.getName())));
 			return true;
 		}
 		return false;
