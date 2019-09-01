d34dd288ac2808b7f7eb9ff2e8de76ada85cdb2e
Simonsator <00pflaume@gmail.com>
2019-09-01 14:22:58 +0200

    Use OnlinePAFplayer instead of PAFPlayer

--- src/main/java/de/simonsator/partyandfriends/api/events/PAFAccountCreateEvent.java
+++ src/main/java/de/simonsator/partyandfriends/api/events/PAFAccountCreateEvent.java
@@ -1,17 +1,16 @@
 package de.simonsator.partyandfriends.api.events;
 
 import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
-import de.simonsator.partyandfriends.api.pafplayers.PAFPlayer;
 import net.md_5.bungee.api.plugin.Event;
 
 public class PAFAccountCreateEvent extends Event {
-	private final PAFPlayer CREATED_ACCOUNT;
+	private final OnlinePAFPlayer CREATED_ACCOUNT;
 
 	public PAFAccountCreateEvent(OnlinePAFPlayer pPlayer) {
 		CREATED_ACCOUNT = pPlayer;
 	}
 
-	public PAFPlayer getCreatedAccount() {
+	public OnlinePAFPlayer getCreatedAccount() {
 		return CREATED_ACCOUNT;
 	}
 }
