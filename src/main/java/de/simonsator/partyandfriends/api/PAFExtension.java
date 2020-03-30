54f4c3cf7b339789672b1ed446e8fd1a1f69d5cd
Simonsator <00pflaume@gmail.com>
2020-03-30 18:01:35 +0200

    Fixed PAFExtension not extending PAFPluginBase

--- src/main/java/de/simonsator/partyandfriends/api/PAFExtension.java
+++ src/main/java/de/simonsator/partyandfriends/api/PAFExtension.java
@@ -1,7 +1,6 @@
 package de.simonsator.partyandfriends.api;
 
 import de.simonsator.partyandfriends.main.Main;
-import net.md_5.bungee.api.plugin.Plugin;
 
 import java.io.File;
 
@@ -9,7 +8,7 @@ import java.io.File;
  * @author Simonsator
  * @version 1.0.0 12.02.17
  */
-public abstract class PAFExtension extends Plugin {
+public abstract class PAFExtension extends PAFPluginBase {
 
 	public void reload() {
 		onDisable();
